eb7a828a3224ce078b9ad3a0e28d726125887673
Jay Hogg <jay.hogg@gcsu.edu>
2025-09-26 09:24:17 -0400

    Trigger redeploy444444444444444455555555557777

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-onst express = require('express');
+const express = require('express');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const authMiddleware = require('../middleware/auth');
